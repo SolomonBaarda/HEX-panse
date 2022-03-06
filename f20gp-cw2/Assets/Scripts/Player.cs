525549e729d9f02f47eaa1b0ecc2f4033af4c947
Solomon Baarda <solomon_baarda@outlook.com>
2022-03-06 17:14:48 +0000

    Added particle system

--- f20gp-cw2/Assets/Scripts/Player.cs
+++ f20gp-cw2/Assets/Scripts/Player.cs
@@ -17,6 +17,9 @@ public class Player : MonoBehaviour
     [Space]
     public TMP_Text StrengthText;
 
+    [Space]
+    public ParticleSystem Particle;
+
     private void Update()
     {
         StrengthText.transform.rotation = Camera.main.transform.rotation;
@@ -44,15 +47,18 @@ public class Player : MonoBehaviour
 
     private IEnumerator MoveThroughPositions(Vector3 start, Vector3 destination, float timeSeconds)
     {
+        Particle.Play();
         float timer = 0;
         Vector3 direction = destination - start;
 
         while (timer < timeSeconds)
         {
             transform.position = start + direction * timer / timeSeconds;
-
+            
             yield return null;
             timer += Time.deltaTime;
         }
+
+        Particle.Stop();
     }
 }
